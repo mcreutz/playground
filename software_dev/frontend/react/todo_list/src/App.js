6d5300de5d4f20fbaf31063c45790c450f127b6f
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-10-14 14:23:26 +0200

    renamed and added comments

--- software_dev/frontend/react/todo_list/src/App.js
+++ software_dev/frontend/react/todo_list/src/App.js
@@ -1,27 +1,35 @@
 // Tutorial from https://www.youtube.com/watch?v=hQAHSlTtcmY
 
-import React from 'react'
-// useState stores currrent state
-// useRef allows access to other components
+// 'useState' stores currrent state ???
+// 'useRef' allows access to other components
+// 'useEfffect' ???
 import { useState, useRef, useEffect } from 'react';
-import TodoList from './TodoList'  // import our component
-import uuidv4 from 'uuid/dist/v4'
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+import TodoList from './TodoList'  // import our own component
+import uuidv4 from 'uuid/dist/v4'  // to create unique IDs for todos
+
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'  // Key to reference our todos in 'useEffect'
+
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
+  // On app load, get stored todos from storage. If not empty, load.
+  // Content of '[]' is monitored for change to trigger this function. No ccontent means triggger on startup.
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if (storedTodos) setTodos(storedTodos)
   }, [])
 
+  // Update local storage of todos, whenenver todos get modified.
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
+
+  // On toggle of chackbox, copy all todos, then toggle '.complete' of given 'id' and update.
   function toggleTodo(id) {
     const newTodos = [...todos]
     const todo = newTodos.find(todo => todo.id === id)
@@ -29,6 +37,7 @@ function App() {
     setTodos(newTodos)
   }
 
+  // On add button click, get input text and add to todos. Then clear input.
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
@@ -38,15 +47,18 @@ function App() {
     todoNameRef.current.value = null
   }
 
+  // On clear button click, filter all todos with .complete false, then update.
   function handleClearTodos() {
     const newTodos = todos.filter(todo => !todo.complete)
     setTodos(newTodos)
   }
 
+
   return (
+    // Encapsulate in empty tag, because 'return' can return only 1 object
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
+      <input ref={todoNameRef} type="text" />  {/* ref to be readable from other component */}
       <button onClick={handleAddTodo}>Add todo</button>
       <button onClick={handleClearTodos}>Clear completed</button>
       <div>There are {todos.filter(todo => !todo.complete).length} todos left</div>
