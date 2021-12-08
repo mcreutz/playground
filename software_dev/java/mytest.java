d8a75b495c2977d934b1223eb65cd4ddf68f2376
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-12-08 15:58:14 +0100

    added java

---
+++ software_dev/java/mytest.java
@@ -0,0 +1,15 @@
+class MyClass {
+    public static void main(String[] args) {
+        int i = 22;
+        if (i == 11) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Fail");
+        }
+        for (int j = 1; j <= 5; j++) {
+            System.out.println(j);
+        }
+        int non_init;
+        System.out.println(non_init + 2);
+    }
+}
