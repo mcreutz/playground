39314b3c4972868da8d28de7b806f53f97eab4a0
Martin <35747507+croidzen@users.noreply.github.com>
2022-04-04 00:58:30 +0200

    updated list_comprehension

--- software_dev/list_comprehension.py
+++ software_dev/list_comprehension.py
@@ -1,6 +1,7 @@
 my_list = []
 for my_element in my_iterable:
     if my_condition:
+        # 'my_expression' usually contains 'my_element'
         my_list.append(my_expression)
 
 my_list = [my_expression for my_element in my_iterable if my_condition]
