4708b284f32f04a798bb0b88bb38f33ff6cc5efe
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-11-17 14:32:49 +0100

    Formatting

--- systems_eng/shellscripting/reference.sh
+++ systems_eng/shellscripting/reference.sh
@@ -7,9 +7,11 @@
 #!/usr/bin/bash
 #!/bin/bash -ex
 
+
 # Output a string
 echo Hello World
 echo "Hello World"
+
 
 # Variables
 MY_VAR = my_value
@@ -20,13 +22,16 @@ echo ${MY_VAR}
 cd "$HOME"/Documents
 cd ${HOME}/Document
 
+
 # Variable scope
 export
+
 
 # Conditional execution
 if <condition>; then
   <commands>
 fi
+
 
 # Loops
 while [condition]; do
@@ -41,39 +46,46 @@ for var in <list>; do  # <list> is a series of strings, separated by spaces: "Re
   <commands>
 done
 
+
 # Concatenation of commands
 command_1 && command_2  # && only executes the second command, if the first one exited with code 1 (means successful)
 command_1 || command_2  # || only executes the second command, if the first one exited with code 0 (means failed)
 command_1 ; command_2  # ; runs both commands in the given order, regardless of the exit codes
 
+
 # Line break
 cd .. \
     && cd ..
 
+
 # Available varibles
 $HOME
+
 
 # comparisons
 ## equality
 ==, !=
-##comparison
+
+## comparison
 <=, >=, <, >
+
 ## bitwise
 &, |, ^(XOR)
+
 ## logical
 &&, ||
+
 
 # evaluations
 (command)
 ((arithmetic expression))
 
-# Slicing
 
+# Slicing
 
 
 # Exit with code
 exit 1  # If no exit command is given at the end and last command was successful, the script will exit with code 1 (means successful)
-
 
 
 # Environment variables
@@ -81,16 +93,12 @@ printenv  # Show all environment variables for the active user, that have a valu
 env  # Run a command under modified environment: $ env EDITOR=vim xterm
 
 
-
-
-
 # Test for availability of features / vars
 test -z "$TARGET_DIR" && { echo "Fatal Error: No TARGET_DIR set" ; exit 1 ; }
 
+
 # Select parent dir
 REPO_ROOT_DIR="${CI_DIR}/.."
-
-
 
 
 # Useful commands and programs
@@ -112,6 +120,7 @@ tar
 tee
 test
 truncate -s 0 filename  # Remove content of file up to given size is reached. Good to clear a file without deleting it.
+
 
 # shortcuts
 !nnn  # Run command no nnn from command history
