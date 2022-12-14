7a549fb22f78c47ab8b7cd1075031e1c9d4c5388
Martin <35747507+croidzen@users.noreply.github.com>
2022-12-15 00:11:03 +0100

    added routing

--- software_dev/javascript/react/reference/src/index.js
+++ software_dev/javascript/react/reference/src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Basics from './Basics';
-import Clock from './Clock';
-import { Form, Toggle, WithParam } from './Events';
-
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // calling root.render() will instanciate the given components and render them
 root.render(
-  <React.StrictMode>
-    <Basics />
-    <Clock />
-    <Form />
-    <Toggle />
-    <br></br>
-    <WithParam />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
