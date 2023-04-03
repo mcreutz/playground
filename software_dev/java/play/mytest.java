3d2394c9e9fa2885bd2f15db894cb94bb3a4c55c
Martin <35747507+croidzen@users.noreply.github.com>
2023-04-03 22:02:15 +0200

    Update java playground

--- software_dev/java/play/mytest.java
+++ software_dev/java/play/mytest.java
@@ -1,4 +1,42 @@
-class MyClass {
+class MyMainClass {
+
+    // properties
+    int x = 5;
+    int y = 6;
+
+
+    // constructor
+    public MyMainClass(int x, int y) {
+        System.out.println("Constructor");
+        this.x = x;
+        this.y = y;
+    }
+
+
+    // getters and setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+    // methods
+    public void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    // main method
     public static void main(String[] args) {
         int i = 22;
         if (i == 11) {
@@ -9,7 +47,10 @@ class MyClass {
         for (int j = 1; j <= 5; j++) {
             System.out.println(j);
         }
-        int non_init;
-        System.out.println(non_init + 2);
+        // int non_init;
+        // System.out.println(non_init + 2);
+
     }
+
+
 }
