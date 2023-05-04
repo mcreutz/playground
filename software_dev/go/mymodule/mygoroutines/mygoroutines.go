2f8afcd7df7cc217b940dd1289607dcf5302053c
Martin <35747507+croidzen@users.noreply.github.com>
2023-05-04 21:36:45 +0200

    Update goroutines

--- software_dev/go/mymodule/mygoroutines/mygoroutines.go
+++ software_dev/go/mymodule/mygoroutines/mygoroutines.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 )
 
+// a worker fucntion that does some work
 func worker(id int, wg *sync.WaitGroup) {
 	for i := 1; i <= 9; i++ {
 		math.Sin(math.Cos(math.Tan(1)))
@@ -16,6 +17,7 @@ func worker(id int, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
+// a go function, that shows the usage of goroutines
 func Goroutines() {
 	fmt.Println("----- Goroutines -----")
 	var wg sync.WaitGroup
@@ -24,4 +26,23 @@ func Goroutines() {
 		go worker(i, &wg)
 	}
 	wg.Wait()
+}
+
+// worker function that waits for a task on a channel and reports a result back
+func worker2(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Printf("Worker %d, job %d\n", id, j)
+		results <- j * 2
+	}
+}
+
+// manager function calling the worker functions
+func manager(jobs chan<- int, results <-chan int) {
+	for i := 1; i <= 9; i++ {
+		jobs <- i
+	}
+	close(jobs)
+	for a := 1; a <= 9; a++ {
+		<-results
+	}
 }
