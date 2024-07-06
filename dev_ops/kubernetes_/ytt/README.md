59e68199ddb493fff54c6bcc1acf1f68a027ffa3
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2024-07-06 03:01:22 +0200

    Add ytt example

---
+++ dev_ops/kubernetes_/ytt/README.md
@@ -0,0 +1,10 @@
+todo:
+upstream manifest offline and online
+values and schema offline and online
+values versioning
+schema versioning
+deploy values to cluster
+deploy external apps by templating into kustomization.yaml
+template top level kustomization.yaml itself
+deploy sequencing
+diff manifests / diff yaml
---
+++ dev_ops/kubernetes_/ytt/render.sh
@@ -0,0 +1,11 @@
+# check if in correct directory
+if [ ! -f render.sh ] || [ ! -f deploy.sh ]; then
+    echo "wrong directory"
+    exit 1
+fi
+
+# delete rendered patches from 'patches_rendered' directory
+rm platform_repo/patches_rendered/*
+
+# render the patch templates
+ytt -f platform_repo/patch_templates -f platform_repo/default_values.yaml --data-values-file cluster_repo/values.yaml --output-files platform_repo/patches_rendered
