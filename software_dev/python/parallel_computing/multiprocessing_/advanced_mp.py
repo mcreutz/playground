b02fc12127188ac79ba6c9b103e1571a5725d0af
Martin <35747507+croidzen@users.noreply.github.com>
2024-10-27 21:39:39 +0100

    Update multiprocessing

--- software_dev/python/parallel_computing/multiprocessing_/advanced_mp.py
+++ software_dev/python/parallel_computing/multiprocessing_/advanced_mp.py
@@ -1,17 +1,45 @@
 import multiprocessing
 import pandas as pd
 
-# multiprocessing.set_start_method('fork', True)
 
-class MyClass():
+def f(x):
+    return pd.DataFrame()
 
-    def __init__(self):
-        pass
 
-    def f(self, x):
-        return pd.DataFrame()
+def multi_f():
+    with multiprocessing.Pool(2) as p:
+        print(p.map(self.f, [1, 2, 3]))
 
-    def multi_f(self, ):
-        with multiprocessing.Pool(2) as p:
-            print(p.map(self.f, [1, 2, 3]))
 
+def square(x):
+    return x * x
+
+
+# link results to initial parameters
+def multi_square():
+    my_nums = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "ten": 10,
+    }
+    my_squares = {}
+    with multiprocessing.Pool() as p:
+        result = p.map(square, my_nums.values())
+    for i, key in enumerate(my_nums.keys()):
+        my_squares[key] = result[i]
+    print(my_squares)
+
+
+if __name__ == "__main__":
+    multi_square()
+
+
+# use imap() for large data. is lazier than map().
+# also use chunksize param to control how many tasks are sent to each process at a time.
